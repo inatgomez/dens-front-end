675586c60f1a605f197925f26134b54f5892b739
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-09-11 12:11:46 -0500

    initial commit

---
+++ writer-den/src/app/placeholder-data.js
@@ -0,0 +1,10 @@
+const ideas = [
+  {
+    title: "meeting in the cabin",
+    content:
+      "Eli sees the cabin from afar, the roof is toppled and smoke is coming out of it. She's afraid her friend is gone, so she walks toward the cabin to find out what happened. When she enters the cabin, Eli finds Bastian on the floor, he has a big bruise in his eye, and blood is oozing out in the floor.",
+    project: "The first keeper",
+    category: "PLOT",
+    date: "09-10-2024",
+  },
+];
