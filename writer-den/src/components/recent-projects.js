447d4be47abadf8df3e1f1955e2e1512a90d7486
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-09-16 11:40:33 -0500

    add initial layout, needs correction

---
+++ writer-den/src/components/recent-projects.js
@@ -0,0 +1,15 @@
+export default function RecentProjects() {
+  return (
+    <div className='grid grid-cols-3 mx-4 my-4 gap-2'>
+      <div className='bg-primary-50 rounded-lg shadow-md max-w-sm overflow-hidden min-h-20'>
+        <h3 className='text-center text-neutral-98 font-bold'>Project</h3>
+      </div>
+      <div className='bg-secondary-50 rounded-lg shadow-md max-w-sm overflow-hidden min-h-20'>
+        <h3 className='text-center text-neutral-98 font-bold'>Project</h3>
+      </div>
+      <div className='bg-tertiary-50 rounded-lg shadow-md max-w-sm overflow-hidden min-h-20'>
+        <h3 className='text-center text-neutral-98 font-bold'>Project</h3>
+      </div>
+    </div>
+  );
+}
--- writer-den/src/pages/index.js
+++ writer-den/src/pages/index.js
@@ -1,9 +1,12 @@
-import Ideacard from "@/components/idea-card";
+import RecentProjects from "@/components/recent-projects";
 
 export default function Home() {
   return (
-    <div className='mx-auto flex justify-center items-center'>
-      <Ideacard />
+    <div className='mt-4 mx-auto flex flex-col justify-around'>
+      <h1 className='font-bold text-center text-neutral-10 text-2xl'>
+        Writer's Den
+      </h1>
+      <RecentProjects />
     </div>
   );
 }
