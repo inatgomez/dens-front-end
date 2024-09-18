e8f175dd3cb0d49ffb6948bf38c59252d27dee5e
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-09-18 15:25:06 -0500

    change navbar to grid and center the search bar

--- writer-den/src/pages/index.js
+++ writer-den/src/pages/index.js
@@ -5,7 +5,7 @@ import SideBar from "@/components/sidebar";
 
 export default function Home() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col min-h-screen'>
       <Navbar />
       <div className='grid grid-cols-5 w-screen'>
         <SideBar className='col-span-1' />
