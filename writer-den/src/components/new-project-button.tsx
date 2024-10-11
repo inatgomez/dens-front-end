b86e636880e0d9c70855404b540af60184b64ce7
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-10-11 11:33:30 -0500

    add event handler to the buttons

--- writer-den/src/components/new-project-button.tsx
+++ writer-den/src/components/new-project-button.tsx
@@ -3,9 +3,15 @@ import { Button } from "@headlessui/react";
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
 export default function NewProjectButton() {
+  function handleClick() {
+    alert("You clicked new project!");
+  }
   return (
     <div className='mt-3 py-1.5 px-3 rounded-md h-8 hover:bg-neutral-50'>
-      <Button className='flex justify-between items-center text-sm font-semibold leading-6 text-neutral-98'>
+      <Button
+        onClick={handleClick}
+        className='flex justify-between items-center text-sm font-semibold leading-6 text-neutral-98'
+      >
         <PlusCircleIcon
           aria-hidden='true'
           className='mr-1 h-5 w-5 text-neutral-98'
