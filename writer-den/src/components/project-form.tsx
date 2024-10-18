a1e8afc06b0fe4f19c27a95cb82a8e756d62b365
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-10-18 09:43:33 -0500

    write form submission function in the project form component

--- writer-den/src/components/project-form.tsx
+++ writer-den/src/components/project-form.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./modal";
 import { useModal } from "@/context/modal-context";
+import { createProject } from "@/services/projectService";
 export default function ProjectForm() {
   const { isProjectModalOpen, closeProjectModal } = useModal();
+
+  const [name, setName] = useState("");
+  const [mainGenre, setMainGenre] = useState("Sci-Fi");
+  const [otherGenre, setOtherGenre] = useState("None");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const projectData = {
+      name,
+      main_genre: mainGenre,
+      mix_genre: otherGenre,
+    };
+
+    try {
+      await createProject(projectData);
+      alert("Project created succesfully!");
+      closeProjectModal();
+    } catch (error) {
+      alert("Failed to create project. Please try again.");
+    }
+  };
 
   return (
     <Modal
@@ -10,7 +33,7 @@ export default function ProjectForm() {
       isOpen={isProjectModalOpen}
       onClose={closeProjectModal}
     >
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8'>
           <div className='sm:col-span-full'>
             <label
@@ -22,16 +45,18 @@ export default function ProjectForm() {
             <div className='mt-2'>
               <input
                 id='project-title'
-                name='project-title'
                 type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 className='block w-full rounded-md border-0 py-1.5 text-neutral-10 shadow-sm ring-1 ring-inset ring-neutral-70 placeholder:text-neutral-40 focus:ring-2 focus:ring-inset focus:ring-primary-40 sm:text-sm sm:leading-6'
+                required
               />
             </div>
           </div>
 
           <div className='sm:col-span-full'>
             <label
-              htmlFor='country'
+              htmlFor='main-genre'
               className='block text-base font-medium leading-6 text-neutral-98'
             >
               Main Genre
@@ -39,7 +64,8 @@ export default function ProjectForm() {
             <div className='mt-2'>
               <select
                 id='main-genre'
-                name='main-genre'
+                value={mainGenre}
+                onChange={(e) => setMainGenre(e.target.value)}
                 className='block w-full rounded-md border-0 py-1.5 text-neutral-10 shadow-sm ring-1 ring-inset ring-neutral-70 placeholder:text-neutral-40 focus:ring-2 focus:ring-inset focus:ring-primary-40 sm:text-sm sm:leading-6'
               >
                 <option>Sci-Fi</option>
@@ -55,7 +81,7 @@ export default function ProjectForm() {
 
           <div className='sm:col-span-full'>
             <label
-              htmlFor='country'
+              htmlFor='other-genre'
               className='block text-base font-medium leading-6 text-neutral-98'
             >
               Other Genre
@@ -63,7 +89,8 @@ export default function ProjectForm() {
             <div className='mt-2'>
               <select
                 id='other-genre'
-                name='other-genre'
+                value={otherGenre}
+                onChange={(e) => setOtherGenre(e.target.value)}
                 className='block w-full rounded-md border-0 py-1.5 text-neutral-10 shadow-sm ring-1 ring-inset ring-neutral-70 placeholder:text-neutral-40 focus:ring-2 focus:ring-inset focus:ring-primary-40 sm:text-sm sm:leading-6'
               >
                 <option>None</option>
