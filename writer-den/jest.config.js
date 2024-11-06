0a1f95c8dd78f5d32e17af5b3861e9b7480df088
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-11-06 13:10:55 -0500

    edit config to add proper paths

--- writer-den/jest.config.js
+++ writer-den/jest.config.js
@@ -1,9 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "json"],
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "node"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
