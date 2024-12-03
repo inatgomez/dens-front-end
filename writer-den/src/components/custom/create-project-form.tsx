503ff4158257eaab98c8e8559462c40d6754bf7e
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-12-03 12:46:06 -0500

    add missing components, start writing reducer to handle project api calls

--- writer-den/src/components/custom/create-project-form.tsx
+++ writer-den/src/components/custom/create-project-form.tsx
@@ -5,18 +5,37 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
-  DialogFooter,
-} from "../ui/dialog";
+} from "@/components/ui/dialog";
 import {
   Select,
   SelectTrigger,
   SelectValue,
   SelectContent,
   SelectItem,
-} from "../ui/select";
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/hooks/use-toast";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import { createProject } from "@/services/projectService";
-import { Button } from "../ui/button";
+
+interface CreateNewProjectFormProps {
+  onSubmit: (project: string[]) => void;
+  children: React.ReactNode;
+}
 
 const GENRES = [
   "ROMANCE",
@@ -31,24 +50,153 @@ const GENRES = [
   "COMEDY",
 ];
 
-export default function CreateNewProject({
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Name must be at least 2 characters",
+    })
+    .max(100),
+  main_genre: z.string(),
+  other_genre: z.string(),
+});
+
+const CreateNewProjectForm: React.FC<CreateNewProjectFormProps> = ({
+  onSubmit,
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}) => {
+  const [name, setName] = React.useState("");
+  const [mainGenre, setMainGenre] = React.useState("");
+  const [otherGenre, setOtherGenre] = React.useState("");
+
+  const { toast } = useToast();
+
+  async function handleSubmit(
+    values: z.infer<typeof formSchema>,
+    e: React.FormEvent
+  ) {
+    e.preventDefault();
+
+    const projectData = {
+      name,
+      main_genre: mainGenre.toUpperCase(),
+      mix_genre: otherGenre === "None" ? "" : otherGenre.toUpperCase(),
+    };
+
+    try {
+      await createProject(projectData);
+      toast({
+        description: "Your project has been created",
+      });
+    } catch (error) {
+      toast({
+        description: "Failed to create project. Try again.",
+      });
+    }
+    console.log(values);
+  }
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      main_genre: "",
+      other_genre: "",
+    },
+  });
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Create new project</DialogTitle>
+          <DialogTitle>Create New Project</DialogTitle>
         </DialogHeader>
-        <DialogFooter>
-          <Button variant='default' size='lg' type='submit'>
-            Save project
-          </Button>
-        </DialogFooter>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className='space-y-8 flex flex-col'
+          >
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Project's Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Name of your new project' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='main_genre'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>What's the main genre?</FormLabel>
+                  <FormControl>
+                    <Select>
+                      <SelectTrigger className='w-1/2'>
+                        <SelectValue
+                          placeholder='Select main genre'
+                          {...field}
+                        />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {GENRES.map((genre) => (
+                          <SelectItem key={genre} value={genre}>
+                            {genre}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='other_genre'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>What's the second genre?</FormLabel>
+                  <FormControl>
+                    <Select>
+                      <SelectTrigger className='w-1/2'>
+                        <SelectValue
+                          placeholder='Select second genre'
+                          {...field}
+                        />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {GENRES.map((genre) => (
+                          <SelectItem key={genre} value={genre}>
+                            {genre}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              variant='default'
+              size='lg'
+              type='submit'
+              className='justify-self-end'
+            >
+              Save project
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
-}
+};
+
+export { CreateNewProjectForm };
--- writer-den/src/components/custom/nav-projects.tsx
+++ writer-den/src/components/custom/nav-projects.tsx
@@ -6,8 +6,15 @@ import {
   SidebarMenuItem,
   SidebarMenuButton,
   SidebarMenuSkeleton,
+  SidebarMenuAction,
 } from "../ui/sidebar";
-import { FileText } from "lucide-react";
+import { FileText, MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 
 interface Project {
   unique_id: string;
@@ -36,6 +43,21 @@ function NavProjects() {
               <span>{project.name || project.message}</span>
             </Link>
           </SidebarMenuButton>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuAction>
+                <MoreHorizontal />
+              </SidebarMenuAction>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side='right' align='start'>
+              <DropdownMenuItem>
+                <span>Edit Project</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Delete Project</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </SidebarMenuItem>
       ))}
     </SidebarMenu>
