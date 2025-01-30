61be568bf652dfea905a1737c81fb922e6c90dea
ngv09 <113272579+ngv09@users.noreply.github.com>
2025-01-30 11:17:14 -0500

    add function to redirect to google auth page

---
+++ writer-den/src/utils/continue-with-social-auth.ts
@@ -0,0 +1,42 @@
+import { useToast } from "@/hooks/use-toast";
+
+export default async function continueWithSocialAuth(
+  provider: string,
+  redirect: string
+) {
+  const { toast } = useToast();
+
+  try {
+    const url = `${
+      process.env.NEXT_PUBLIC_HOST
+    }/api/o/${provider}/redirect_uri=${
+      process.env.NODE_ENV === "production"
+        ? process.env.NEXT_PUBLIC_REDIRECT_URL
+        : "http://127.0.0.1:8000"
+    }/auth/${redirect}`;
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+      credentials: "include",
+    });
+
+    const data = await response.json();
+
+    if (response.status === 200 && typeof window !== "undefined") {
+      window.location.replace(data.authorization_url);
+    } else {
+      toast({
+        description: "Something went wrong.",
+        variant: "destructive",
+      });
+    }
+  } catch (error) {
+    toast({
+      description: "Something went wrong.",
+      variant: "destructive",
+    });
+  }
+}
