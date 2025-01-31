1966a791db23ea7a84534913da6ce5cd5e7f6b38
ngv09 <113272579+ngv09@users.noreply.github.com>
2025-01-31 09:20:56 -0500

    center loading message

--- writer-den/src/pages/auth/google.tsx
+++ writer-den/src/pages/auth/google.tsx
@@ -1,18 +1,16 @@
 import { useGoogleAuthenticateMutation } from "@/redux/features/authApiSlice";
 import useSocialAuth from "@/hooks/use-social-auth";
 
-import { Card, CardDescription, CardHeader } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Google() {
   const [googleAuthenticate] = useGoogleAuthenticateMutation();
   useSocialAuth(googleAuthenticate);
 
   return (
-    <div className='flex flex-col gap-6'>
+    <div className='flex justify-center items-center gap-6'>
       <Card>
-        <CardHeader>
-          <CardDescription>Loading sign in page</CardDescription>
-        </CardHeader>
+        <CardContent>Loading page</CardContent>
       </Card>
     </div>
   );
