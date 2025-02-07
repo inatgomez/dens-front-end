4a3acfba52dd5efc30ecf48080736e47bb504fd7
ngv09 <113272579+ngv09@users.noreply.github.com>
2025-02-07 11:07:45 -0500

    delete unnecessary code

--- writer-den/src/components/custom/layout.tsx
+++ writer-den/src/components/custom/layout.tsx
@@ -1,4 +1,3 @@
-// import { cookies } from "next/headers";
 import {
   SidebarInset,
   SidebarProvider,
@@ -7,8 +6,6 @@ import {
 import { Toaster } from "@/components/ui/toaster";
 import { AppSidebar } from "./app-sidebar";
 export default function Layout({ children }: { children: React.ReactNode }) {
-  // const cookieStore = await cookies();
-  // const defaultOpen = cookieStore.get("sidebar:state")?.value === "true";
   return (
     <SidebarProvider>
       <AppSidebar />
